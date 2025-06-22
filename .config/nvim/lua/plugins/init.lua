b99f8925c510255133c59276b431997f435d94d0
Git-Cat-21 <khushmish21@gmail.com>
2025-06-22 17:38:38 +0530

    vim be better ;)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,4 +25,12 @@ return {
   -- 		},
   -- 	},
   -- },
+{
+  'szymonwilczek/vim-be-better',
+  lazy=false,
+  config = function()
+    -- Optional: Enable logging for debugging
+    vim.g.vim_be_better_log_file = 1
+  end
+},
 }
