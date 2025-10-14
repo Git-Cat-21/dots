ffec5f063f28f43276c92aa6c4e1c0a1ec644b29
Git-Cat-21 <khushmish21@gmail.com>
2025-10-14 08:35:36 +0530

    md and pyright :)

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -33,4 +33,15 @@ return {
     vim.g.vim_be_better_log_file = 1
   end
 },
+{
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft={ "markdown" },
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+},
 }
+
